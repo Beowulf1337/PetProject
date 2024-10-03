cee16dae241c724663b953451364113d50ebf5d9
Beowulf1337 <oliver.mantorp@gmail.com>
2024-10-03 14:33:18 +0200

    commented tiny amount of code in main.jsx

--- PetProgram.Web/src/main.jsx
+++ PetProgram.Web/src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// create the root of the app and render the App component within StrictMode
 createRoot(document.getElementById('root')).render(
+
+   /* Wrap the app in StrictMode for highlighting issues */
   <StrictMode>
+
+    {/* Render the main App component */}
     <App />
   </StrictMode>,
 )
