15ef17341e4ffb6b600845361eb3c665fbfe8584
Beowulf1337 <oliver.mantorp@gmail.com>
2024-10-03 15:39:07 +0200

    commented the last to files left Main.js and Pet.js

--- PetProgram.App/Main.js
+++ PetProgram.App/Main.js
@@ -1,4 +1,5 @@
 import { registerRootComponent } from 'expo';
 import App from './App';
 
+// register the App component as the root component which will be rendered first when the app starts
 registerRootComponent(App);
